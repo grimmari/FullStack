2d5de3ab6f95685828c1583db7e69852a9f687b9
grimmari <arip.grimm@gmail.com>
2019-03-20 11:25:10 +0200

    Osa1.2

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,30 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
 
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>yhteensä {exercises1 + exercises2 + exercises3} tehtävää</p>
-    </div>
-  )
+
+const App = () => {
+    const course = 'Half Stack -sovelluskehitys'
+    const part1 = 'Reactin perusteet'
+    const exercises1 = 10
+    const part2 = 'Tiedonvälitys propseilla'
+    const exercises2 = 7
+    const part3 = 'Komponenttien tila'
+    const exercises3 = 14
+
+    const Header = () => {
+        return (
+            <h1>Half Stack -sovelluskehitys</h1>
+        )
+    }
+    const Content = (props) => {
+        const part = props.part
+        const excercises = props.excercises
+        return (
+            <div>
+                <Part part={part1} exercises={exercises1} />
+                <Part part={part2} exercises={exercises2} />
+                <Part part={part3} exercises={exercises3} />
+
+            </div>
+
+        )
+    }
+    const Total = () => {
+        return (
+            <p>yhteensä {exercises1 + exercises2 + exercises3} tehtävää</p>
+        )
+    }
+
+    const Part = (props) => {
+        return (
+            <p>
+                {props.part} {props.exercises}
+            </p>
+        )
+    }
+    return (
+        <div>
+            <Header course={course} />
+            <Content />
+            <Total />
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
