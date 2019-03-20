175f178d9ae83a5f4a9fc4a70a35273f28d96d7d
grimmari <arip.grimm@gmail.com>
2019-03-20 11:49:48 +0200

    osa1.3

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,15 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-
 const App = () => {
     const course = 'Half Stack -sovelluskehitys'
-    const part1 = 'Reactin perusteet'
-    const exercises1 = 10
-    const part2 = 'Tiedonvälitys propseilla'
-    const exercises2 = 7
-    const part3 = 'Komponenttien tila'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Reactin perusteet',
+        exercises: 10
+      }
+      const part2 = {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      }
+      const part3 = {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
 
     const Header = () => {
         return (
@@ -18,24 +23,24 @@ const App = () => {
         )
     }
     const Content = (props) => {
-        const part = props.part
-        const excercises = props.excercises
+        
         return (
             <div>
-                <Part part={part1} exercises={exercises1} />
-                <Part part={part2} exercises={exercises2} />
-                <Part part={part3} exercises={exercises3} />
-
+                <Part part={part1.name} exercises={part1.exercises} />
+                <Part part={part2.name} exercises={part2.exercises} />
+                <Part part={part3.name} exercises={part3.exercises} />
+    
             </div>
-
+    
         )
     }
-    const Total = () => {
+    const Total = (props) => {
+        
         return (
-            <p>yhteensä {exercises1 + exercises2 + exercises3} tehtävää</p>
+            <p>yhteensä {part1.exercises + part2.exercises + part3.exercises} tehtävää</p>
         )
     }
-
+    
     const Part = (props) => {
         return (
             <p>
@@ -43,6 +48,8 @@ const App = () => {
             </p>
         )
     }
+    
+    
     return (
         <div>
             <Header course={course} />
